aa5d008f2ad19dd81a0e47533aa0dac35435ad69
Sopsy <721951+Sopsy@users.noreply.github.com>
2018-10-16 13:58:34 +0200

    Add more tests to cover different padding lengths

--- tests/DecodeTest.php
+++ tests/DecodeTest.php
@@ -7,21 +7,37 @@ final class DecodeTest extends TestCase
 {
     public function testWithPadding()
     {
-        $encoded = 'V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBhIHdhdGVybWVsb24=';
+        // No padding characters
+        $this->assertEquals(
+            'When I grow up, I want to be something!',
+            \Sopsy\Base64Url\Base64Url::decode('V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBzb21ldGhpbmch')
+        );
 
+        // One padding character
         $this->assertEquals(
             'When I grow up, I want to be a watermelon',
-            \Sopsy\Base64Url\Base64Url::decode($encoded)
+            \Sopsy\Base64Url\Base64Url::decode('V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBhIHdhdGVybWVsb24=')
+        );
+
+        // Two padding characters
+        $this->assertEquals(
+            'When I grow up, I want to be anything',
+            \Sopsy\Base64Url\Base64Url::decode('V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBhbnl0aGluZw==')
         );
     }
 
     public function testWithoutPadding()
     {
-        $encoded = 'V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBhIHdhdGVybWVsb24';
-
+        // One padding character stripped
         $this->assertEquals(
             'When I grow up, I want to be a watermelon',
-            \Sopsy\Base64Url\Base64Url::decode($encoded)
+            \Sopsy\Base64Url\Base64Url::decode('V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBhIHdhdGVybWVsb24')
+        );
+
+        // Two padding characters stripped
+        $this->assertEquals(
+            'When I grow up, I want to be anything',
+            \Sopsy\Base64Url\Base64Url::decode('V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBhbnl0aGluZw')
         );
     }
 }
--- tests/EncodeTest.php
+++ tests/EncodeTest.php
@@ -7,21 +7,43 @@ final class EncodeTest extends TestCase
 {
     public function testWithPadding()
     {
-        $input = 'When I grow up, I want to be a watermelon';
+        // No padding characters
+        $this->assertEquals(
+            'V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBzb21ldGhpbmch',
+            \Sopsy\Base64Url\Base64Url::encode('When I grow up, I want to be something!', false)
+        );
 
+        // One padding character
         $this->assertEquals(
             'V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBhIHdhdGVybWVsb24=',
-            \Sopsy\Base64Url\Base64Url::encode($input, false)
+            \Sopsy\Base64Url\Base64Url::encode('When I grow up, I want to be a watermelon', false)
+        );
+
+        // Two padding characters
+        $this->assertEquals(
+            'V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBhbnl0aGluZw==',
+            \Sopsy\Base64Url\Base64Url::encode('When I grow up, I want to be anything', false)
         );
     }
 
     public function testWithoutPadding()
     {
-        $input = 'When I grow up, I want to be a watermelon';
+        // No padding characters
+        $this->assertEquals(
+            'V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBzb21ldGhpbmch',
+            \Sopsy\Base64Url\Base64Url::encode('When I grow up, I want to be something!')
+        );
 
+        // One padding character
         $this->assertEquals(
             'V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBhIHdhdGVybWVsb24',
-            \Sopsy\Base64Url\Base64Url::encode($input)
+            \Sopsy\Base64Url\Base64Url::encode('When I grow up, I want to be a watermelon')
+        );
+
+        // Two padding characters
+        $this->assertEquals(
+            'V2hlbiBJIGdyb3cgdXAsIEkgd2FudCB0byBiZSBhbnl0aGluZw',
+            \Sopsy\Base64Url\Base64Url::encode('When I grow up, I want to be anything')
         );
     }
 }
